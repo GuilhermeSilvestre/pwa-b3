4c766f0c5a7b55e3b255e55a9a49c972fa1ec37f
Guilherme Silvestre <100291684+GuilhermeSilvestre@users.noreply.github.com>
2026-06-05 22:13:08 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'b3-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './dados.json',
+  './icon.png',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os arquivos no cache do celular
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativa e remove caches antigos, se houver
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Serve os arquivos direto do celular para abrir instantaneamente
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
